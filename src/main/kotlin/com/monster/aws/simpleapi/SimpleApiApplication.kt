c845185ba2f4043b79dc959a6621e24a4e6e8989
Balaji Sanjeevi <balajisanjeevi@gmail.com>
2020-10-07 15:25:48 +0800

    adding additional tag - application

--- src/main/kotlin/com/monster/aws/simpleapi/SimpleApiApplication.kt
+++ src/main/kotlin/com/monster/aws/simpleapi/SimpleApiApplication.kt
@@ -4,6 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import io.micrometer.core.instrument.MeterRegistry
 import io.micrometer.core.instrument.Tag
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer
 import org.springframework.context.annotation.Bean
 
@@ -11,11 +12,15 @@ import org.springframework.context.annotation.Bean
 @SpringBootApplication
 class SimpleApiApplication {
 
+    @Value("\${spring.application.name}")
+    lateinit var application: String
+
     @Bean
     fun metricsCommonTags(): MeterRegistryCustomizer<MeterRegistry> =
             MeterRegistryCustomizer {
                 it.config().commonTags(listOf(
-                        Tag.of("env", "local")
+                        Tag.of("env", "local"),
+                        Tag.of("application", application)
                 ))
             }
 
